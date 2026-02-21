0c3a041bc8e45495225aeed8ee771956b60c8470
Bhanu Reddy <bhanuprakashreddyvangala@gmail.com>
2026-02-21 19:09:05 +0530

    updated for sending the otp from send grid service

--- src/main/java/com/finance/personalfinancemanager/service/EmailService.java
+++ src/main/java/com/finance/personalfinancemanager/service/EmailService.java
@@ -2,10 +2,12 @@ package com.finance.personalfinancemanager.service;
 
 
 import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
@@ -25,13 +27,15 @@ public class EmailService {
 
     public void sendOTPEmail(String toEmail,String otp) {
         try {
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setFrom("Finance Manager <bhanuprakashreddyvangala@gmail.com>");
-            mailMessage.setTo(toEmail);
-            mailMessage.setSubject("Finance Manager: Email Verification");
-            mailMessage.setText("Your OTP code is: " + otp + "\n\nThis code will expire in 5 minutes.\n\nIf you didn't request this, please ignore this email.");
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-            mailSender.send(mailMessage);
+            helper.setFrom("Finance Manager <bhanuprakashreddyvangala@gmail.com>");
+            helper.setTo(toEmail);
+            helper.setSubject("Finance Manager: Email Verification");
+            helper.setText("Your OTP code is: " + otp + "\n\nThis code will expire in 5 minutes.\n\nIf you didn't request this, please ignore this email.");
+
+            mailSender.send(message);
         }
         catch (Exception e) {
             throw new RuntimeException(toEmail);
